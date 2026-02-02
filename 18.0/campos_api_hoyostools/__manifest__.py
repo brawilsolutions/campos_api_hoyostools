20948578754e630935083799b7a71e40c9b94139
brawilsolutions <gerente1@brawilsolutions.com>
2026-02-02 10:05:11 -0500

    Agregar módulo Campos API para Productos con campos personalizados y vista heredada

---
+++ 18.0/campos_api_hoyostools/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Campos API para Productos",
+    "version": "18.0.1.0",
+    "summary": "Campos creados para la integración vía API con Distribuciones Hoyostools.",
+    "description": """
+Modulo que añade campos personalizados a los productos para facilitar la integración vía API con Distribuciones Hoyostools.
+""",
+    "author": "Distribuciones Hoyostools",
+    "website": "https://wwww.hoyostools.com",
+    "category": "Inventory",
+    "license": "LGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        'views/product_template_views.xml',
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
+
